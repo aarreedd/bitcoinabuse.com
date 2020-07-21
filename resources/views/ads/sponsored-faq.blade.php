72178757ca7dc2f9ebe608c2106c197f6b812520
V <victor@blockchaingroup.io>
2020-07-21 11:22:45 -0700

    📚 FAQ: Q1: How can Blockchain Intelligence Group help

--- resources/views/ads/sponsored-faq.blade.php
+++ resources/views/ads/sponsored-faq.blade.php
@@ -8,3 +8,14 @@
 <p>
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
 </p> --}}
+
+
+{{-- Blockchain Intelligence Group FAQ Start Here  --}}
+<h4 class="mt-5">
+	How can Blockchain Intelligence Group help? 
+	<span class="badge badge-secondary">Sponsored</span>
+</h4>
+<p>
+	Visually track, trace and monitor Bitcoin transactions with Blockchain Intelligence Group's forensics tool <a href="https://qlue.io/qlue/?utm_source=bitcoinabuse&utm_medium=faq" target="_blank" rel="sponsored">QLUE</a>. Get a crypto equivalent of a credit score for the addresses you deal with and follow the money behind these transactions. Blockchain Intelligence Group offers a <a href="http://https://bigcs.com/bitrankqlue/?utm_source=bitcoinabuse&utm_medium=faq" target="_blank" rel="sponsored">compliance suite</a> that allow for sophisticated cryptocurrency forensics and real-time risk mitigation. For more info, contact us at <a href="mailto:info@blockchaingroup.io" rel="sponsored">info@blockchaingroup.io</a>. 
+</p>
+{{-- end  --}}
